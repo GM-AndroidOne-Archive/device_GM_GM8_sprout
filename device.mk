2fde7427f80dd95c0706bb6eaefb4c24170a83ee
ygityanik <yeyanik123@gmail.com>
2021-06-06 14:36:46 +0300

    GM8_sprout: anasını siktiğimin keymasteri

--- device.mk
+++ device.mk
@@ -323,16 +323,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# Gatekeeper 
-PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service \
-
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
-	
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \

36afd3a8230a28ab3e1ac170580ddea20032c0c4
ygityanik <yeyanik123@gmail.com>
2021-07-15 04:16:24 +0300

    GM8_sprout: import framework detect from LA.UM.8.6.r1-02300-89xx.0

--- device.mk
+++ device.mk
@@ -388,6 +388,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# Framework detect
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
